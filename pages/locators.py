3c4b09e1ec801529cd3a9430c6e2701873f59b5c
EisenhornI <Ig0rS@yandex.ru>
2022-01-17 23:27:21 +0300

    Добавлены тесты страницы логина

---
+++ pages/locators.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    login_link = (By.CSS_SELECTOR, "#login_link")
+class LoginPageLocators():
+    register_form = (By.CSS_SELECTOR, "#register_form")
+    login_form = (By.CSS_SELECTOR, "#login_form")
